38c49d6652c1577883ec98e09cde963da0027143
Priyanka <priyankab813@gmail.com>
2026-06-23 22:57:33 +0530

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,30 @@
+# Variable examples
+
+name = "Priyanka"      # String (str)
+age = 20               # Integer (int)
+height = 5.4           # Float (float)
+is_student = True      # Boolean (bool)
+
+# Printing variables
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+print("Student:", is_student)
+
+# Checking data types
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_student))
+
+student_name = "Priyanka"
+marks = 85
+percentage = 85.5
+subjects = ["Python", "HTML", "Java"]
+details = {"Course": "BCA", "Year": 3}
+
+print(student_name)
+print(marks)
+print(percentage)
+print(subjects)
+print(details)
