681e7d2295916fb2e2b9b772f20df016abe87f8f
Priyanka <priyankab813@gmail.com>
2026-06-23 23:07:01 +0530

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,61 @@
+# Arithmetic Operators
+a = 10
+b = 3
+
+print("Addition:", a + b)        # 13
+print("Subtraction:", a - b)     # 7
+print("Multiplication:", a * b)  # 30
+print("Division:", a / b)        # 3.333...
+print("Floor Division:", a // b) # 3
+print("Modulus:", a % b)         # 1
+print("Exponent:", a ** b)       # 1000
+
+#Comparison Operators
+x = 10
+y = 20
+
+print(x == y)  # False
+print(x != y)  # True
+print(x > y)   # False
+print(x < y)   # True
+print(x >= y)  # False
+print(x <= y)  # True
+
+#Assignment Operators
+x = 10
+
+x += 5
+print(x)  # 15
+
+x -= 3
+print(x)  # 12
+
+x *= 2
+print(x)  # 24
+
+x /= 4
+print(x)  # 6.0 
+
+#Logical Operators
+a = True
+b = False
+
+print(a and b)  # False
+print(a or b)   # True
+print(not a)    # False
+
+#Identity Operators
+x = 10
+y = 10
+z = 20
+
+print(x is y)  # True
+print(x is z)  # False
+print(x is not z)  # True
+
+#Membership Operators
+x = [1, 2, 3, 4, 5]
+print(3 in x)   # True
+print(6 in x)   # False
+print(6 not in x)  # True
+
